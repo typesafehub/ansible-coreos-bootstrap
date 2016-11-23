0cc32f00f5387c84a0bf95a403243b56b5bb9658
tedder <ted@timmons.me>
2016-11-22 16:36:59 -0800

    update to pypy 5.6, smarter pip installation
    - update from 5.1.0 to 5.6.0 (current). File naming scheme changed, instead of pypy-VERSION it is pypy2-vVERSION.
    - change installation process to remove any existing pypy directory. Otherwise reinstalls fail. (this sets up for #25 nicely)
    - use ensurepip instead of get-pip.py. One less call. As a side effect the `copy` of get-pip was failing on Ansible v2.2.0, this fi
    xes #32.
    - add `PYTHONPATH` to the python wrapper. Allows pypy to use the shipped modules (like `ensurepip`).

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -8,16 +8,18 @@ if [[ -e $HOME/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=5.1.0
+PYPY_VERSION=5.6.0
 
-if [[ -e $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2 ]]; then
-  tar -xjf $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2
-  rm -rf $HOME/pypy-$PYPY_VERSION-linux64.tar.bz2
+if [[ -e $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2 ]]; then
+  tar -xjf $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2
+  rm -rf $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2
 else
-  wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
+  wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy2-v$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
 fi
 
-mv -n pypy-$PYPY_VERSION-linux64 pypy
+# remove, because otherwise this version will get placed into a subdir of tan existing pypy/
+rm -rf pypy
+mv -f pypy2-v$PYPY_VERSION-linux64 pypy
 
 ## library fixup
 mkdir -p pypy/lib
@@ -27,7 +29,7 @@ mkdir -p $HOME/bin
 
 cat > $HOME/bin/python <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
+PYTHONPATH=/home/core/pypy/lib-python/2.7/:$PYTHONPATH LD_LIBRARY_PATH=$HOME/pypy/lib:$LD_LIBRARY_PATH exec $HOME/pypy/bin/pypy "\$@"
 EOF
 
 chmod +x $HOME/bin/python
