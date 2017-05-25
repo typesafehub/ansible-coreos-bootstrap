1c76af6d12d7fd360563011f2b47316dad3a41b6
Brendan McAdams <brendan@lightbend.com>
2017-05-25 08:51:41 -0700

    Update to Python 3

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -8,13 +8,13 @@ if [[ -e /home/lightbend/pypy/bin/pypy ]]; then
   exit 0
 fi
 
-PYPY_VERSION=5.6
-
-if [[ -e $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2 ]]; then
-  tar -xjf $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2
-  rm -rf $HOME/pypy2-v$PYPY_VERSION-linux64.tar.bz2
+PYPY_VERSION=3.5-5.7.1-beta
+pypy3.5-5.7.1-beta-linux_x86_64-portable.tar.bz2
+if [[ -e $HOME/pypy$PYPY_VERSION-linux_x86_64-portable.tar.bz2 ]]; then
+  tar -xjf $HOME/pypy$PYPY_VERSION-linux_x86_64-portable.tar.bz2
+#  rm -rf $HOME/pypy$PYPY_VERSION-linux_x86_64-portable.tar.bz2
 else
-  wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
+  wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
 fi
 
 # remove, because otherwise this version will get placed into a subdir of an existing pypy/
@@ -24,6 +24,8 @@ mv -f pypy-$PYPY_VERSION-linux_x86_64-portable /home/lightbend/pypy
 ## library fixup
 mkdir -p /home/lightbend/pypy/lib
 ln -snf /lib64/libncurses.so.5.9 /home/lightbend/pypy/lib/libtinfo.so.5
+
+chown -R lightbend /home/lightbend/pypy
 
 /home/lightbend/pypy/bin/pypy --version
 /home/lightbend/pypy/bin/pypy -m ensurepip
